23c53d91039b5dea45a8c27ae657795e087e9e60
@Rakesh953 <rakeshmeher953@gmail.com>
2024-08-21 12:21:36 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+import datetime
+
+def filters(request):
+    dt=datetime.datetime.now()
+    d={'data':'this IS BUIlt iN FIlters','dt':dt,'c':1}
+    return render(request,'filters.html',d)
+
+def user(request):
+    d={'data':'TOday We ARE dealing with Userdefined FilTERs'}
+    return render(request,'user.html',d)
